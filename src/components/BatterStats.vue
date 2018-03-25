34973e8c20bfa65eec3ead773bca533cce66f9f6
silvia <silvia_777@live.cn>
2018-03-25 19:17:27 -0400

    Sub-component: Batter Stats

---
+++ src/components/BatterStats.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-if="batters">{{batters}}</div>
+</template>
+
+<script>
+  export default {
+    name: "batter-stats",
+    props: {
+      batters: {
+        type: Array,
+        default: () => []
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
