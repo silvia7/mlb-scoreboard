90935a3caeef91e91b5f01628c7cb851b4922f55
silvia <silvia_777@live.cn>
2018-03-25 00:19:34 -0400

    initial commit

---
+++ src/components/ScoreBoard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="wrapper">
+    <h1 class="app-name">{{ appName }}</h1>
+    <h2> content TBD </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ScoreBoard',
+  data () {
+    return {
+      appName: 'MLB Scoreboard'
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+.app-name {
+  font-weight: bold;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+</style>
