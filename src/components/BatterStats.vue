698d4af7fab10e92466a4dfb2e82d281276e6a50
silvia <silvia_777@live.cn>
2018-03-25 19:41:04 -0400

    Format batter stats into a table display

--- src/components/BatterStats.vue
+++ src/components/BatterStats.vue
@@ -1,5 +1,26 @@
 <template>
-  <div v-if="batters">{{batters}}</div>
+  <div v-if="batters">
+    <div class="row label">
+      <span class="name">Name</span>
+      <span class="stat">AB</span>
+      <span class="stat">R</span>
+      <span class="stat">H</span>
+      <span class="stat">RBI</span>
+      <span class="stat">BB</span>
+      <span class="stat">SO</span>
+      <span class="stat">AVG</span>
+    </div>
+    <div class="row batter" v-for="batter in batters">
+      <span class="name">{{ batter.name_display_first_last }}</span>
+      <span class="stat">{{ batter.ab || 0 }}</span>
+      <span class="stat">{{ batter.rh || 0 }}</span>
+      <span class="stat">{{ batter.r || 0 }}</span>
+      <span class="stat">{{ batter.rbi || 0 }}</span>
+      <span class="stat">{{ batter.bb || 0 }}</span>
+      <span class="stat">{{ batter.so ||0 }}</span>
+      <span class="stat">{{ batter.avg || 0}}</span>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -15,5 +36,17 @@
 </script>
 
 <style scoped>
+  .label {
+    font-weight: bold;
+  }
 
+  .row .name {
+    display: inline-block;
+    width: 20%;
+  }
+
+  .row .stat {
+    display: inline-block;
+    width: 10%;
+  }
 </style>
