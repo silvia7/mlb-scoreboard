36af85dc685c3a05d6b13203a141d1ea1b6bb912
silvia <silvia_777@live.cn>
2018-03-25 17:45:15 -0400

    Link to new detail view from list view

--- src/components/ScoreBoardListView.vue
+++ src/components/ScoreBoardListView.vue
@@ -4,17 +4,19 @@
 
     <div v-if="data">
       <div class="game" v-for="game of games">
-        <div v-bind:class="{ winner: game.homescore > game.awayscore }" class="team">
-          {{ game.home_team_name }}
-          <span v-if="game.homescore">{{ game.homescore }}</span>
-        </div>
+        <router-link :to="{name: 'ScoreBoardDetailView', params:{ gameDataDir: game.game_data_directory}}">
+          <div v-bind:class="{ winner: game.homescore > game.awayscore }" class="team">
+            {{ game.home_team_name }}
+            <span v-if="game.homescore">{{ game.homescore }}</span>
+          </div>
 
-        <div v-bind:class="{ winner: game.homescore < game.awayscore }" class="team">
-          {{ game.away_team_name }}
-          <span v-if="game.awayscore">{{ game.awayscore }}</span>
-        </div>
+          <div v-bind:class="{ winner: game.homescore < game.awayscore }" class="team">
+            {{ game.away_team_name }}
+            <span v-if="game.awayscore">{{ game.awayscore }}</span>
+          </div>
 
-        <div class="game-status" >{{ game.status.status }}</div>
+          <div class="game-status" >{{ game.status.status }}</div>
+        </router-link>
       </div>
     </div>
 
@@ -43,7 +45,7 @@ export default {
       .then(data => {
         this.data = data;
         this.games = ( Array.isArray(data.game) ) ? data.game:[data.game];
-
+        console.log(data);
         this.games.forEach(game => {
           if ( game.linescore && game.linescore.r ) {
             game.homescore = Number(game.linescore.r.home);
